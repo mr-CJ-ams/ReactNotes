ac738341fc0564abb14b7911ffb9e1f9822992c9
Carlo Amaquin <amaquincj01@gmail.com>
2024-08-07 21:41:41 +0800

    Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
-import { Form } from "./components/Form";
+import { useToggle } from "./components/useToggle";
 
 function App() {
+
+  const [isVisible, toggle] = useToggle(false);
+  const [isVisible2, toggle2] = useToggle(false);
   return (
     <div className="App">
-      <Form />
+      <button onClick={toggle} >
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h1>Hidden Text</h1> }
+
+      <button onClick={toggle2} >
+        {isVisible2 ? "Hide" : "Show"}
+      </button>
+      {isVisible2 && <h1>Hidden Text</h1> }
     </div>
   );
 }
---
+++ src/components/useToggle.js
@@ -0,0 +1,12 @@
+import { useContext, useEffect, useState } from "react";
+
+export const useToggle = (initialVal = false) => {
+    const [state, setState] = useState(false);
+
+    const toggle = () => {
+        setState((prev) => !prev)
+    }
+
+    return [state, toggle];
+
+};
