d0c2648ff14660713e53d2dc35e514cff984c73b
Carlo Amaquin <amaquincj01@gmail.com>
2024-08-08 22:12:39 +0800

    React Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Person } from "./components/Person";
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person 
+        name="Carlo"
+        email="amaquincj00@gmail.com"
+        age={21}
+        isMarried={false}
+        friends={["ricmar","ghea","roxan","justine","paul"]}
+      />
     </div>
   );
 }
